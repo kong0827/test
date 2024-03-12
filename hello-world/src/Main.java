8257e6f1fb4589bb2f264e48e0a1de02d42ecfa8
kongxiangjin <1351882069@qq.com>
2024-03-12 10:06:45 +0800

    fix

--- hello-world/src/Main.java
+++ hello-world/src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("lisa append!");
+        System.out.println("kxj append!");
     }
 }
